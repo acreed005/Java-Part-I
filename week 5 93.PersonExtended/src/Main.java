63833ec3d002af752e80ea66cd1f8d76738422cc
acreed005 <48038713+acreed005@users.noreply.github.com>
2019-03-09 20:44:50 -0500

    Create Main.java

---
+++ week 5 93.PersonExtended/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    Person peter = new Person ("Peter", 15, 2, 1993);
+    Person bb = new Person ("B", 12, 12, 2012); 
+    
+      System.out.println(bb.olderThan(peter));
+    
+    // Person pekka = new Person("Pekka", 15, 2, 1993);
+  }
+}
