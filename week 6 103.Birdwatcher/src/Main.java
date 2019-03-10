b8bce1c7bb6d8c5bc626b0ac334dc3b243a05751
acreed005 <48038713+acreed005@users.noreply.github.com>
2019-03-10 19:11:54 -0400

    Create Main.java

---
+++ week 6 103.Birdwatcher/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+// Design & implement an observation database for a bird watcher
+// Database contains birds, each of which have a name & a Latin name (both Strings)
+// Database also tracks how many times each bird has been observed
+
+// only 1 scanner obj 
+// no static variables     
+
+public class Main {  
+
+    public static void main(String[] args) {
+        userInput b = new userInput(); 
+        
+        b.start(); 
+    }
+}
